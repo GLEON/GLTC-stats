620777a87e6787ebe9ab9632d687f511b6cb7662
Jordan S Read <jread@usgs.gov>
2012-12-29 14:01:31 -0600

    Modifications to fit to previous year for JFM

--- defaultsGLTC.m
+++ defaultsGLTC.m
@@ -1,11 +1,18 @@
-timeRange = 'JAS';
+timeRange = 'JFM';
 plotSumm  = true;           % plot summary figure?
-mmS = [7 8 9];
-toolMmS = [6 7 8];
-fitRange = [153 305];       % June 1 to Oct 31st
-toolFitRange = [122 274];   % May 1 to Sept 30th
 
-JFM_offset = 183;           % days offset for JFM calcs
+if strcmp(timeRange,'JAS')
+    mmS = [7 8 9];
+    toolMmS = [6 7 8];
+    fitRange = [153 305];       % June 1 to Oct 31st
+    toolFitRange = [122 274];   % May 1 to Sept 30th
+elseif strcmp(timeRange,'JFM')
+    mmS = [1 2 3];
+    toolMmS = [1 2 3];
+    fitRange = [-30 121];       % Dec 1 to April 30th
+    toolFitRange = [-30 121];   % Dec 1 to April 30th
+end
+
 mxTemp = 45;                % degrees C, for outlier removal
 
 dateForm = 'yyyy-mm-dd HH:MM:SS';
