91b673dcfd7a55cd8ad2df94a3ceea27043be77b
Jordan S Read <jread@usgs.gov>
2012-12-29 13:28:20 -0600

    use date bump instead of window shift

--- defaultsGLTC.m
+++ defaultsGLTC.m
@@ -1,19 +1,11 @@
-
 timeRange = 'JAS';
 plotSumm  = true;           % plot summary figure?
+mmS = [7 8 9];
+toolMmS = [6 7 8];
+fitRange = [153 305];       % June 1 to Oct 31st
+toolFitRange = [122 274];   % May 1 to Sept 30th
 
-if strcmp(timeRange,'JAS')
-    mmS = [7 8 9];
-    toolMmS = [6 7 8];
-    fitRange = [153 305];       % June 1 to Oct 31st
-    toolFitRange = [122 274];   % May 1 to Sept 30th
-elseif strcmp(timeRange,'JFM')
-    mmS = [1 2 3];
-    toolMmS = [1 2 3];
-    fitRange = [153 305];       % June 1 to Oct 31st
-    toolFitRange = [122 274];   % May 1 to Sept 30th
-end
-
+JFM_offset = 183;           % days offset for JFM calcs
 mxTemp = 45;                % degrees C, for outlier removal
 
 dateForm = 'yyyy-mm-dd HH:MM:SS';
