3dbb9b10c3e7c90fc6a1e91695c69634b7e80acf
Jordan S Read <jread@usgs.gov>
2013-01-01 09:15:37 -0600

    code cleaning

--- runUncertainty.m
+++ runUncertainty.m
@@ -1,31 +1,24 @@
 function runUncertainty
-
 % writes all files for raw data in directory.
 % ** does not include Toolik **
 
 defaultsGLTC
 
-
 %% file finder
-
 availfiles = dir(fullfile(rootDir));
-% remove directories from this structure
 numFiles = length(availfiles);
 rmvFile = false(numFiles,1);
 for k = 1:numFiles
     if availfiles(k).isdir
-        rmvFile(k) = true;
+        rmvFile(k) = true;  % remove directories from this structure
     else
         disp(availfiles(k).name);
     end
 end
-
 availfiles = availfiles(~rmvFile);      % now only files
-
 %% loop through files, read out, parse and write stats
 
 numFiles = length(availfiles);
-[fileN] = startLog;
 for k = 1:numFiles
     fileName = availfiles(k).name;
     disp('******-------********');
@@ -49,27 +42,16 @@ for k = 1:numFiles
             [ fitParams, R2 ] = fitDayNum( datesZ, wtrZ, fitRange,timeRange);
             [ years, meVal, mxGap, meGap, nmGap, logMessage ] = ...
                 getStats( datesZ, wtrZ, mmS, fitParams, R2);
-
-                plotSummaryFig(fitParams,R2,years,meVal,logMessage,...
-                    datesZ,wtrZ,unLakes{lk},zBest); pause(0.5);
-
         end
     else
         zBest = getBestDepth(dates,z);
         useI = eq(zBest,zT);
         datesZ = dates(useI);
         wtrZ   = wtr(useI);
-        % find depth with most years, tiebreaker: shallowest depth
-        
         [ fitParams, R2 ] = fitDayNum( datesZ, wtrZ, fitRange,timeRange);
         [ years, meVal, mxGap, meGap, nmGap, logMessage ] = ...
             getStats( datesZ, wtrZ, mmS, fitParams, R2);
-        plotSummaryFig(fitParams,R2,years,meVal,logMessage,...
-            datesZ,wtrZ,lakeNm,zBest); pause(0.5);
-        
     end
-    
 end
-closeLog(fileN)
 end
 
