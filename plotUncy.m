05f01f435e156d7da0e7c80dd575629811bbfa8c
Jordan S Read <jread@usgs.gov>
2013-01-06 14:31:53 -0600

    plot stats update

--- plotUncy.m
+++ plotUncy.m
@@ -4,7 +4,9 @@ function plotUncy(dates,wtr,mmS,fitParams,R2,clr)
 defaultsGLTC;
 YYYY = datevec(min(dates));
 YYYY = YYYY(1);
-% [~,meVal] = getStats( dates, wtr, mmS, fitParams, R2); % initial value
+strYr = num2str(YYYY);
+disp(strYr);
+[~,meVal] = getStats( dates, wtr, mmS, fitParams, R2); % initial value
 
 useI = ge(dates,datenum(YYYY,mmS(1),1)) & ...
     lt(dates,datenum(YYYY,mmS(3)+1,1));
@@ -16,8 +18,8 @@ rngL = (100-confInt)/2;
 
 indx = 1:length(dates);
 indx = indx(useI); % only indices of values that fall within JAS
-uncy = NaN(initNum-endValsUncy,1);
-xVal = uncy;
+uncy = NaN(initNum-endValsUncy,2);
+xVal = NaN(initNum-endValsUncy,1);
 for bs = 1:initNum-endValsUncy
     xVal(bs) = initNum-bs;
     
@@ -32,10 +34,12 @@ for bs = 1:initNum-endValsUncy
         [~,yVal(n)] = getStats( dates(tempI), wtr(tempI), mmS, fitParams, R2);
     end
     range = prctile(yVal,[rngL 100-rngL]);
-    uncy(bs) = (range(2)-range(1))*0.5;
-    
+    uncy(bs,1) = meVal-range(1);
+    uncy(bs,2) = meVal-range(2);
 end
-plot([initNum; xVal],[0; uncy],'b-','Color',clr,'LineWidth',lW);  
-
+plot([initNum; xVal],[0; uncy(:,1)],'b-','Color',clr,'LineWidth',lW);  
+plot([initNum; xVal],[0; uncy(:,2)],'b-','Color',clr,'LineWidth',lW); 
+%text(initNum,meVal,[' ' strYr],'HorizontalAlignment','left',...
+%    'FontName',fontN,'FontSize',8);
 end
 
