538015f25f1260a820f21df1d5a4c731bae1e7bf
Jordan S Read <jread@usgs.gov>
2013-01-13 10:46:38 -0600

    Better "best depth" algorithm

--- getBestDepth.m
+++ getBestDepth.m
@@ -2,7 +2,7 @@ function zBest = getBestDepth(dates,z)
 
 % returns the "best" depth to use for lake. Will return the most sampled
 % depth that is also the shallowest as a tiebreaker
-
+defaultsGLTC
 % sort just in case
 [z,srtI] = sort(z);
 dates = dates(srtI);
@@ -12,7 +12,9 @@ for i = 1:length(unZ)
     useI = eq(z,unZ(i));
     datesTemp = dates(useI);
     yr = datevec(datesTemp);
-    yrCnt(i) = length(unique(yr(:,1)));
+    useI = eq(yr(:,2),mmS(1)) | eq(yr(:,2),mmS(2)) | eq(yr(:,2),mmS(3));
+    
+    yrCnt(i) = length(unique(yr(useI,1)));
 end
 
 [~,mxI] = max(yrCnt);
